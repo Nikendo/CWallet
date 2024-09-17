9ffe7276dde82b735b96c06299ec0797ee52ee5c
Nikita Shmatov <pravdomir42@gmail.com>
2024-09-17 17:55:42 +0300

    Initial Commit

---
+++ CWallet/CWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWalletApp.swift
+//  CWallet
+//
+//  Created by Shmatov Nikita on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
